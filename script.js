31e947417e35a6ada6d215a9b5e62ea9a5982e02
balapavithra <pavikannan2991@gmail.com>
2023-07-25 20:38:33 +0530

    longest string

---
+++ script.js
@@ -0,0 +1,14 @@
+let word = findLongestWord(["Ed", " black", "flux", "Technology's"]);
+function findLongestWord(array) {
+    var longestWord = "";
+  
+    array.forEach(function(word) {
+      if(word.length > longestWord.length) {
+        longestWord = word;
+      }
+    });
+  
+    return longestWord;
+  }
+  
+  console.log(word);
